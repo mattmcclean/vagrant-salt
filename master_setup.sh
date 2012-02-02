2ced07e16ed7dad60f8abc0a32d1cc480ac964d5
Matthew McClean <matt.mcclean@gmail.com>
2012-02-02 16:01:46 +0400

    Added config files for each VM instead of copying from tempate

--- master_setup.sh
+++ master_setup.sh
@@ -2,16 +2,9 @@
 
 echo 'Setting up Salt Master'
 
-echo 'Setting up Salt Master config file'
-cd /etc/salt
-sudo sed -e 's/#auto_accept: False/auto_accept: True/g' master.template | sudo tee master > /dev/null
-sudo sed -i -e 's/#open_mode: False/open_mode: True/g' master
-sudo sed -i -e 's/#file_roots:/file_roots:/g' master
-sudo sed -i -e 's/#  base:/  base:/g' master
-sudo sed -i -e 's/#    - \/srv\/salt/    - \/vagrant\/srv\/salt/g' master
-
-cd /home/vagrant
+echo 'Copying Salt Master config file to VM'
+sudo cp /vagrant/master.conf /etc/salt/master
 
 echo 'Starting Salt Master process'
-nohup sudo salt-master --log-level=debug > /var/log/salt-master.log 2>&1 &
-echo 'Salt Master process started. For debug log goto: /var/log/salt-master.log'
+nohup sudo salt-master &
+echo 'Salt Master process started'
--- minion_setup.sh
+++ minion_setup.sh
@@ -5,20 +5,18 @@ die () {
 }
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 
-echo 'Setting up Salt Minion'
+min_name=`hostname -s`
+echo 'Setting up Salt Minion with name: ' $min_name 
 
 echo 'Salt Master IP address is ' $1
 
-cd /etc/salt
-sudo sed 's/#master: salt/master: '${1}'/g' minion.template | sudo tee minion > /dev/null
-cd /home/vagrant
+[ -f /vagrant/$min_name.conf ] || die "File: /vagrant/$min_name.conf does not exist"
+
+sudo sed 's/#master: salt/master: '${1}'/g' /vagrant/$min_name.conf | sudo tee /etc/salt/minion > /dev/null
 
 echo 'Starting Salt Minion'
-nohup sudo salt-minion --log-level=debug > /var/log/salt-minion.log 2>&1 &
-echo 'Salt Minion started. For debug log goto: /var/log/salt-minion.log'
-
-#echo 'Sleeping for 5 seconds to allow minion to properly connect with master'
-#sleep 5
+nohup sudo salt-minion &
+echo 'Salt Minion started'
 
 echo 'Call state.highstate command to configure minion'
 sudo salt-call state.highstate
